e0451238d74a2031a882d70254ddd55395e255e3
Huang Yaming <yumminhuang@gmail.com>
2016-11-14 17:47:12 +0800

    Update dotfiles
    Signed-off-by: Huang Yaming <yumminhuang@gmail.com>

--- files/homebrew.sh
+++ files/homebrew.sh
@@ -6,7 +6,7 @@ set -e
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install must-have homebrew packages
-packages=( hugo tig httpie tree htop tmux )
+packages=( htop httpie tig tmux tree )
 
 for i in "${packages[@]}"
 do
