8fd7ec7bdfb8059378fb2b3b2967dfa48f57fee1
Huang Yaming <yumminhuang@users.noreply.github.com>
2020-11-10 20:57:27 +0800

    Update homebrew.sh

--- homebrew.sh
+++ homebrew.sh
@@ -3,12 +3,11 @@
 set -e
 
 # Install Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # Install must-have homebrew packages
-packages=( diff-so-fancy htop httpie jq tig tmux tree youtube-dl yq )
+brew install ansible awscli azure-cli diff-so-fancy httpie ipcalc jq oath-toolkit tig tmux traefik wget yamllint yq
+brew cask install docker multipass session-manager-plugin
 
-for i in "${packages[@]}"
-do
-    brew install $i
-done
+# Install Hashicorp softwares
+brew install hashicorp/tap/consul hashicorp/tap/nomad hashicorp/tap/packer hashicorp/tap/vault
